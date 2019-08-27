d3d71319c4a4b3bb23e43b64d8ac5e7141af03c9
David Cornish <52015556+dcornish84@users.noreply.github.com>
2019-08-27 13:44:02 -0500

    Chicken Monkey Practice time

---
+++ chickenMonkey.js
@@ -0,0 +1,13 @@
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    if (currentNumber % 5 === 0 && currentnumber %7 ==0) {
+        console.log("ChickenMonkey");
+
+    }  else if (currentnumber % 5 == 0) {
+        console.log("Chicken");
+        
+    }  else if (currentnumber % 7 == 0) {
+        console.log("Monkey");
+    }  else {
+        (console.log(currentNumber))
+    }
+}
